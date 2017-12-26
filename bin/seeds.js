112069127f9aced204431211f1f85125bad19f77
Imran Syed <syed.imran2512@gmail.com>
2017-12-25 21:28:40 -0500

    seeds

---
+++ bin/seeds.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+const country = require('../models/countries');
+const pad = require('../models/padModel');
+const travel = require('../models/travelModel');
