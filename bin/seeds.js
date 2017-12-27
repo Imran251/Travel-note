65c4a155864978a52422268aaf713a70530760b6
Imran Syed <syed.imran2512@gmail.com>
2017-12-26 23:41:18 -0500

    seeds done

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,3 +3,5 @@ const Schema   = mongoose.Schema;
 const country = require('../models/countries');
 const pad = require('../models/padModel');
 const travel = require('../models/travelModel');
+const user = require('../models/user');
+const ObjectId  = require('mongoose').Types.ObjectId;
