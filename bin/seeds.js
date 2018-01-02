dcf2ef20a7c8461a4ccfe32d9db7b0bdd34ab984
Imran Syed <syed.imran2512@gmail.com>
2018-01-01 23:35:19 -0500

    done

--- bin/seeds.js
+++ bin/seeds.js
@@ -21,6 +21,19 @@ const countries = [
      "Kenya"
 ];
 
+const pads = [
+{
+     title: "Heidelberg",
+     description: "My amazing journey to Heidelberg",
+    //  location: [8.672434,49.398750],
+     country:"Germany",
+     pic_name:"001.jpg",
+     pic_path:"/uploads/001",
+     visible: true,
+     travelId  : ObjectId('591434a7b3f2f3440f1628d5')
+}
+];
+
 user.create(users, (err, docs) => {
   if (err) {
     throw err;
